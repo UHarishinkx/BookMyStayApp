1812587a7500611786908dd8548d8f275b756ad9
Harish U <harishinkx@gmail.com>
2026-03-11 09:11:17 +0530

    Basic Room Types 459

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,4 +1,48 @@
-void main(){
-    System.out.println("Welcome to the hotel Booking Management System");
-    System.out.println("System initialized successfully.");
+abstract class Room {
+    protected String roomType;
+    protected int price;
+    protected int availableRooms;
+
+    public Room(String roomType, int price, int availableRooms) {
+        this.roomType = roomType;
+        this.price = price;
+        this.availableRooms = availableRooms;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Price per Night: ₹" + price);
+        System.out.println("Available Rooms: " + availableRooms);
+        System.out.println("");
+    }
+}
+class SingleRoom extends Room {
+
+    public SingleRoom(int availableRooms) {
+        super("Single Room", 2000, availableRooms);
+    }
+}
+class DoubleRoom extends Room {
+
+    public DoubleRoom(int availableRooms) {
+        super("Double Room", 3500, availableRooms);
+    }
+}
+class SuiteRoom extends Room {
+
+    public SuiteRoom(int availableRooms) {
+        super("Suite Room", 5000, availableRooms);
+    }
+}
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        Room single = new SingleRoom(10);
+        Room doubleRoom = new DoubleRoom(6);
+        Room suite = new SuiteRoom(3);
+        System.out.println("Welcome to BookMyStay\n");
+        single.displayDetails();
+        doubleRoom.displayDetails();
+        suite.displayDetails();
+    }
 }
