3394409a0341644346e48fe61d75be7b58dae47d
Harish U <harishinkx@gmail.com>
2026-03-18 10:06:57 +0530

    UC-11 Concurrent Booking

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -2,9 +2,7 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        double amount = 2000;
-
-        System.out.println("Processing payment: " + amount);
-        System.out.println("Payment Successful");
+        System.out.println("Sending booking confirmation...");
+        System.out.println("Notification sent successfully");
     }
 }
