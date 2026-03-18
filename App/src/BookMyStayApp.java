4ac9a9fcb963d48001fef09bd2ade17ed284c956
Harish U <harishinkx@gmail.com>
2026-03-18 10:11:55 +0530

    UC12 Data Perisistence

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,8 +1,31 @@
+import java.util.*;
+
+class Reservation {
+    String guest, room;
+
+    Reservation(String g, String r) {
+        guest = g;
+        room = r;
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Sending booking confirmation...");
-        System.out.println("Notification sent successfully");
+        Queue<Reservation> queue = new LinkedList<>();
+        queue.add(new Reservation("Abhi", "Single"));
+
+        Map<String, Integer> inventory = new HashMap<>();
+        inventory.put("Single", 1);
+
+        while (!queue.isEmpty()) {
+            Reservation r = queue.poll();
+
+            if (inventory.get(r.room) > 0) {
+                inventory.put(r.room, inventory.get(r.room) - 1);
+                System.out.println("Booking confirmed for " + r.guest);
+            }
+        }
     }
 }
